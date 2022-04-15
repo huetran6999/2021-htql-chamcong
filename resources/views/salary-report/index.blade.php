e5c47643ca782ac59bace698731cde1aaa5e80d6
Hue Tran <tranvnh99@gmail.com>
2022-04-15 09:31:28 +0700

    commit HT

--- resources/views/salary-report/index.blade.php
+++ resources/views/salary-report/index.blade.php
@@ -1,47 +1,57 @@
 @extends('layout.index')
 @section('content')
 <div class="container">
-    <form action="{{ route('salaryReport.index')}}" method="get">
+    <form action="{{ route('salaryReport.index')}}" method="get" style="padding-top: 56px" class="row g-3">
         @csrf
-        <label for="years">Năm:</label>
-        <select name="years" id="years">
+        <div class="col-md-5 form-inline">
+        {{-- <label for="years" class="form-label">Năm:</label> --}}
+        <select name="years" id="years" class="form-select">
             <option disabled selected>--Chọn Năm--</option>
             @foreach ($years as $year)
             <option value="{{ $year }}">{{ $year }}</option>
             @endforeach
         </select>
+        </div>
 
-        <label for="months">Tháng:</label>
-        <select name="months" id="months">
+        <div class="col-md-5">
+        {{-- <label for="months" class="form-label">Tháng:</label> --}}
+        <select name="months" id="months" class="form-select">
             <option disabled selected>--Chọn tháng--</option>
             @foreach ($months as $month)
             <option value="{{ $month }}">{{ $month }}</option>
             @endforeach
         </select>
+        </div>
 
-        <input type="submit" value="Submit">
+        <div class="col-md-2">
+            <input type="submit" value="Duyệt" class="btn btn-primary">
+        </div>    
+
+        
     </form>
-    <table class="table">
+    <table class="table table-hover">
         <thead>
             <tr>
-                <th>Employee</th>
-                <th>Salary</th>
-                <Th>Date</Th>
+                <th>STT</th>
+                <th>Nhân viên</th>
+                <th>Lương</th>
+                <Th>Ngày</Th>
             </tr>
         </thead>
         <tbody>
             @foreach ($users as $user)
             @php
-            $salary = $user->user->position->basic_salary * $user->user->salary->coefficient_salary ;
-            $socialInsurance = $salary * 10.5 / 100;
-            $tax = $salary * 10 / 100;
+            $salary = $user->user->position->basic_salary * $user->user->salary->coefficient_salary ; //gross-salary
+            $socialInsurance = $salary * 10.5 / 100; //bảo hiểm xã hội
+            $tax = $salary * 10 / 100; //thuế
             @endphp
             <tr>
+                <td>{{ $loop-> index + 1 }}</td>
                 <td>{{ $user->user->username }} - {{ $user->user->u_name }}</td>
                 @if ($user->month === 2)
-                <td>đ {{ number_format((($salary * $user->total / 24) - $socialInsurance - $tax), 0, ',', ',') }}</td>
+                <td> {{ number_format((($salary * $user->total / 24) - $socialInsurance - $tax), 0, ',', ',') }} VNĐ</td>
                 @else
-                <td>đ {{ number_format((($salary * $user->total / 26) - $socialInsurance - $tax), 0, ',', ',') }}</td>
+                <td> {{ number_format((($salary * $user->total / 26) - $socialInsurance - $tax), 0, ',', ',') }} VNĐ</td>
                 @endif
                 <Th>{{ $user->month }}-{{ $user->year }}</Th>
             </tr>
